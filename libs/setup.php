dbe3bd3cb6d41672f359000dd727e72a4c1bec3b
Raxso <sopcam@hotmail.com>
2023-02-09 00:34:58 +0100

    v002

--- libs/setup.php
+++ libs/setup.php
@@ -8,7 +8,8 @@ $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 // Parámetros de conexión a la BD
-$config->set('dbhost', 'containers-us-west-189.railway.app:7409');
+//mysql://root:jxrs1vvt8ZyodJjr9RXU@containers-us-west-189.railway.app:7409/railway
+$config->set('dbhost', 'jxrs1vvt8ZyodJjr9RXU@containers-us-west-189.railway.app:7409/railway');
 $config->set('dbname', 'railway');
 $config->set('dbuser', 'root');
 $config->set('dbpass', 'jxrs1vvt8ZyodJjr9RXU');
